587d4d5dc80d66f94feca23fa28eb82503c965f1
= <=dohaiyen0306@gmail.com>
2020-10-21 09:12:21 +0700

    as

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Session;
+use Illuminate\support\Facades\Redirect;
+session_start();
 
 class CartController extends Controller
 {
-    //
+    public function save_cart(Request $request){
+        $cate_product = DB::table('tbl_category_product')->where('category_status','0')->orderby('category_id','desc')->get();
+        $brand_product = DB::table('tbl_brand')->where('brand_status','0')->orderby('brand_id','desc')->get();
+        $productId = $request->productid_hidden;
+        $quantity = $request->qty;
+        $product_info = DB::table('tbl_product')->where('product_id',$productId)->first();
+        Cart::add('293ad', 'Product 1', 1, 9.99, 550);
+            
+        return view('pages.cart.show_cart')->with('category',$cate_product)->with('brand',$brand_product);
+    }
 }
